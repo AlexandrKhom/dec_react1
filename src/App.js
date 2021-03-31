f2b9763d40306ab63eb64704ebfdb248985c1876
AlexandrKhom <alex988andr@gmail.com>
2021-04-01 01:14:23 +0300

    default

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
+import React, {Component} from 'react';
 
-import './App.css';
-import User from "./components/user/User";
-
-function App() {
-
-    let users = [
-        {name: `alex`, age: 22, status: true},
-        {name: `ira`, age: 11, status: false},
-        {name: `max`, age: 33, status: true},
-        {name: `anna`, age: 44, status: false},
-        {name: `ander`, age: 99, status: true},
-    ]
-    return (
-        <div>
-
-            {
-                users.map((value, index)=> <User user={value} key={index}/>)
-            }
-
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
 }
 
 export default App;
